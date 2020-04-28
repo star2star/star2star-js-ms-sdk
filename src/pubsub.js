608fbc7123af8c02a3c1ed501b0588b59a128d05
Nathan Harris <nharris@star2star.com>
2020-04-28 14:07:49 -0400

    add new pubusub endpoints

--- src/pubsub.js
+++ src/pubsub.js
@@ -65,6 +65,222 @@ const addSubscription = async (
 
 /**
  * @async
+ * @description This function will add a subscription for a custom application and event.
+ * @param {string} [accessToken="null accessToken"] - access token for cpaas systems
+ * @param {string} [app_uuid="app_uuid uuid not provided "] - account to subscribe to
+ * @param {string} [callback_url="not set callback"] - callback URL
+ * @param {array} [callback_headers=[]] - callback headers
+ * @param {array} [criteria=[]] - filter criteria
+ * @param {object} [subscriptions={}] - events to subscribe to (voice, fax, conferencing, messagin, sms,  presence)
+ * @param {string} [expiresDate=undefined] - optional expires date (RFC3339 format)
+ * @param {object} [trace = {}] - optional microservice lifecycle trace headers
+ * @returns {Promise<object>} - Promise resolving to a data object containing a new subscription
+ */
+const addCustomEventSubscription = async (
+  accessToken = "null accessToken",
+  app_uuid = "account uuid not provided ",
+  callback_url = "not set callback",
+  callback_headers = [],
+  criteria = [],
+  subscriptions = {},
+  expiresDate = undefined,
+  trace = {}
+) => {
+  try {
+    const MS = util.getEndpoint("pubsub");
+    
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-type": "application/json",
+        "x-api-version": `${util.getVersion()}`
+      },
+      uri: `${MS}/customevents`,
+      body: {
+        app_uuid: app_uuid,
+        callback: {
+          url: callback_url,
+          headers: callback_headers
+        },
+        criteria: criteria,
+        events: subscriptions
+      },
+      json: true
+    };
+    if (expiresDate) {
+      requestOptions.body.expiration_date = expiresDate;
+    }
+    util.addRequestTrace(requestOptions, trace);
+    const response = await request(requestOptions);
+    return response;
+  } catch (error) {
+    return Promise.reject(util.formatError(error));
+  }
+};
+
+/**
+ * @async
+ * @description - This function will send a message to subscribers of a custom application
+ * @param {string} [accessToken="null accessToken"] - CPaaS access token
+ * @param {string} [app_uuid="account uuid not provided "] - application uuid
+ * @param {string} [event="null event"] - even string to broadcast
+ * @param {*} [trace={}] - optional CPaaS lifecycle headers
+ * @returns {Promise} - promise resolving to success or failure
+ */
+const broadcastCustomApplication = async (
+  accessToken = "null accessToken",
+  app_uuid = "account uuid not provided ",
+  event = "null event",
+  trace = {}
+) => {
+  try {
+    const MS = util.getEndpoint("pubsub");
+    const requestOptions = {
+      "method": "POST",
+      "headers": {
+        "Authorization": `Bearer ${accessToken}`,
+        "Content-type": "application/json",
+        "x-api-version": `${util.getVersion()}`
+      },
+      "uri": `${MS}/broadcasts/applications`,
+      "body": {
+        "app_uuid": app_uuid,
+        "event": event
+      },
+      "json": true
+    };   
+    util.addRequestTrace(requestOptions, trace);
+    const response = await request(requestOptions);
+    return response;
+  } catch (error) {
+    return Promise.reject(util.formatError(error));
+  }
+};
+
+/**
+ * @async
+ * @description - This function will create a custom pubsub application
+ * @param {string} [accessToken="null accessToken"] - CPaaS access token
+ * @param {string} [app_uuid="account uuid not provided "] - application_uuid
+ * @param {array} [events=[]] - events as array of objects
+ * @param {object} [trace={}] - optional CPaaS lifecycle headers
+ * @returns {Promise} - promise resolving to success or failure
+ */
+const createCustomApplication = async (
+  accessToken = "null accessToken",
+  app_uuid = "account uuid not provided ",
+  events = [],
+  trace = {}
+) => {
+  try {
+    const MS = util.getEndpoint("pubsub");
+    const requestOptions = {
+      "method": "POST",
+      "headers": {
+        "Authorization": `Bearer ${accessToken}`,
+        "Content-type": "application/json",
+        "x-api-version": `${util.getVersion()}`
+      },
+      "uri": `${MS}/applications`,
+      "body": {
+        "app_uuid": app_uuid,
+        "events": events
+      },
+      "json": true
+    };   
+    util.addRequestTrace(requestOptions, trace);
+    const response = await request(requestOptions);
+    return response;
+  } catch (error) {
+    return Promise.reject(util.formatError(error));
+  }
+};
+
+/**
+ * @async
+ * @desc - This function will delete a custom pubsub application
+ * @param {string} [accessToken="null accessToken"] - CPaaS access token
+ * @param {string} [app_uuid="account uuid not provided "] - application uuid
+ * @param {*} [trace={}] - optional CPaaS lifecycle headers
+ * @returns {Promise} - promise resolving to success or failure
+ */
+const deleteCustomApplication = async (
+  accessToken = "null accessToken",
+  app_uuid = "account uuid not provided ",
+  trace = {}
+) => {
+  try {
+    const MS = util.getEndpoint("pubsub");
+    const requestOptions = {
+      "method": "DELETE",
+      "headers": {
+        "Authorization": `Bearer ${accessToken}`,
+        "Content-type": "application/json",
+        "x-api-version": `${util.getVersion()}`
+      },
+      "uri": `${MS}/applications/${app_uuid}`,
+      "json": true
+    };   
+    util.addRequestTrace(requestOptions, trace);
+    const response = await request(requestOptions);
+    return response;
+  } catch (error) {
+    return Promise.reject(util.formatError(error));
+  }
+};
+
+/**
+ * @async
+ * @description - This function will delete a custom subscription
+ * @param {string} [accessToken="null accessToken"]
+ * @param {string} [subscription_uuid="no subscription uuid provided"]
+ * @param {*} [trace={}]
+ * @returns {Promise} - promise resolving to success or failure
+ */
+const deleteCustomSubscription = async (
+  accessToken = "null accessToken",
+  subscription_uuid = "no subscription uuid provided",
+  trace = {}
+) => {
+  try {
+    const MS = util.getEndpoint("pubsub");
+
+    const requestOptions = {
+      method: "DELETE",
+      uri: `${MS}/customevents/${subscription_uuid}`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-type": "application/json",
+        "x-api-version": `${util.getVersion()}`
+      },
+      resolveWithFullResponse: true,
+      json: true
+    };
+    util.addRequestTrace(requestOptions, trace);
+    const response = await request(requestOptions);
+    if (response.hasOwnProperty("statusCode") && response.statusCode === 204) {
+      return {"status": "ok"};
+    } else {
+      // this is an edge case, but protects against unexpected 2xx or 3xx response codes.
+      throw {
+        "code": response.statusCode,
+        "message": typeof response.body === "string" ? response.body : "delete pubsub failed",
+        "trace_id": requestOptions.hasOwnProperty("headers") && requestOptions.headers.hasOwnProperty("trace")
+          ? requestOptions.headers.trace 
+          : undefined,
+        "details": typeof response.body === "object" && response.body !== null
+          ? [response.body]
+          : []
+      };
+    } 
+  } catch (error) {
+    return Promise.reject(util.formatError(error));
+  }
+};
+
+/**
+ * @async
  * @description This function will delete a user subscriptions based on subscription id.
  * @param {string} [subscription_uuid="no subscription uuid provided"] - uuid for a star2star user
  * @param {string} [accessToken="null accessToken"] - access token for cpaas systems
@@ -107,6 +323,72 @@ const deleteSubscription = async (
           : []
       };
     } 
+  } catch (error) {
+    return Promise.reject(util.formatError(error));
+  }
+};
+
+/**
+ * @async
+ * @description - This function will return a custom pubsub application
+ * @param {string} [accessToken="null accessToken"] - CPaaS access token
+ * @param {string} [app_uuid="account uuid not provided "] - application uuid
+ * @param {*} [trace={}] - optional CPaaS lifecycle headers
+ * @returns {Promise} - promise resolving to custom application object
+ */
+const getCustomApplication = async (
+  accessToken = "null accessToken",
+  app_uuid = "account uuid not provided ",
+  trace = {}
+) => {
+  try {
+    const MS = util.getEndpoint("pubsub");
+    const requestOptions = {
+      "method": "GET",
+      "headers": {
+        "Authorization": `Bearer ${accessToken}`,
+        "Content-type": "application/json",
+        "x-api-version": `${util.getVersion()}`
+      },
+      "uri": `${MS}/applications/${app_uuid}`,
+      "json": true
+    };   
+    util.addRequestTrace(requestOptions, trace);
+    const response = await request(requestOptions);
+    return response;
+  } catch (error) {
+    return Promise.reject(util.formatError(error));
+  }
+};
+
+/**
+ * @async
+ * @description - This function will return a custom subscription
+ * @param {string} [accessToken="null accessToken"]
+ * @param {string} [subscription_uuid="no subscription uuid provided"]
+ * @param {*} [trace={}]
+ * @returns {Promise} - promise resolving to subscription object
+ */
+const getCustomSubscription = async (
+  accessToken = "null accessToken",
+  subscription_uuid = "no subscription uuid provided",
+  trace = {}
+) => {
+  try {
+    const MS = util.getEndpoint("pubsub");
+    const requestOptions = {
+      method: "GET",
+      uri: `${MS}/customevents/${subscription_uuid}`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-type": "application/json",
+        "x-api-version": `${util.getVersion()}`
+      },
+      json: true
+    };
+    util.addRequestTrace(requestOptions, trace);
+    const response = await request(requestOptions);
+    return response;
   } catch (error) {
     return Promise.reject(util.formatError(error));
   }
@@ -224,7 +506,14 @@ const updateSubscriptionExpiresDate = async (
 
 module.exports = {
   addSubscription,
+  addCustomEventSubscription,
+  broadcastCustomApplication,
+  createCustomApplication,
+  deleteCustomApplication,
+  deleteCustomSubscription,
   deleteSubscription,
+  getCustomApplication,
+  getCustomSubscription,
   getSubscription,
   listUserSubscriptions,
   updateSubscriptionExpiresDate 
